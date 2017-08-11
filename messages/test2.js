1b123b310fba2950e68f33b2bb24727c62179628
SamuelSJTU <wangzhh1994@163.com>
2017-08-11 17:15:13 +0800

    repair

--- messages/test2.js
+++
@@ -1,6 +0,0 @@
-var GAS = require('./getAnswerSync.js');
-var sl = require('./syncLuis.js');
-var question  = "人与室内环境连之伟教室在哪";
-console.log(GAS.getIntentAndEntities(question));
-
-// console.log(sl.getIntentLuis(question)[0]);
